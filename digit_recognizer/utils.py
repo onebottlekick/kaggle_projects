a1a70bd6d59e35a41b766f3c219e2d85b4267f0a
onebottlekick <gksqudcks97@gmail.com>
2021-12-25 21:02:21 +0900

    added history plot function

--- digit_recognizer/utils.py
+++ digit_recognizer/utils.py
@@ -1,6 +1,44 @@
+import os
 import pickle
 
-def load_data():
+import matplotlib.pyplot as plt
+
+def load_data(flatten=True):
     with open('datasets/data.pkl', 'rb') as f:
         data = pickle.load(f)
-    return data
+    if flatten:
+        return data
+    else:
+        for i in range(3):
+            data[i] = (data[i]/255.0).reshape(-1, 28, 28, 1)
+        return data
+
+def plot_history(history,  fig_name, save=False):
+    train_history = history.history['loss']
+    validation_history = history.history['val_loss']
+    fig = plt.figure(figsize=(15, 8))
+    plt.subplot(1, 2, 1)
+    plt.title('Loss History')
+    plt.xlabel('EPOCH')
+    plt.ylabel('LOSS Function')
+    plt.plot(train_history, 'red', label='train')
+    plt.plot(validation_history, 'blue', label='validation')
+    plt.grid(True)
+    plt.legend()
+
+    train_history = history.history['accuracy']
+    validation_history = history.history['val_accuracy']
+    plt.subplot(1, 2, 2)
+    plt.title('Accuracy History')
+    plt.xlabel('EPOCH')
+    plt.ylabel('Accuracy')
+    plt.plot(train_history, 'red', label='train')
+    plt.plot(validation_history, 'blue', label='validation')
+    plt.grid(True)
+    plt.legend()
+    
+    if save:
+        os.makedirs('train_plots', exist_ok=True)
+        fig.savefig(f"train_plots/{fig_name}_history.png")
+
+    plt.show()
